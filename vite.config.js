76dd64aa9fc15a590f22e390bee9ee9172a7c709
Muhammad-hassan31144 <31144@students.riphah.edu.pk>
2025-07-18 21:20:46 +0500

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
+if (!global.crypto) {
+  global.crypto = require('crypto');
+  global.crypto.getRandomValues = (arr) => require('crypto').randomFillSync(arr);
+}
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
